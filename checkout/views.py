4a677aace84f59e927b65e34174b85bc8e3040bf
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-03-08 18:11:31 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MjQeTDE6aRQG5DREe8sa9dRkEDJlL7JQs56zI3SPf03goPXEIsse10WNquzBM0bjXo35YMWZDHXj48U0cw0tob700Gak3fFt1'
     }
 
     return render(request, template, context)
