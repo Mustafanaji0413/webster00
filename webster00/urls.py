d89d32ff11488c1562419926d01d06fe3260ba6a
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-03-22 20:34:48 +0000

    Adding custom 404 page

--- webster00/urls.py
+++ webster00/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +14,7 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('contact/', include('contact.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'webster00.views.handler404'
