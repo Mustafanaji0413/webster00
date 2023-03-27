fd56d228cf84f1ee3100559cddda53d9ed438574
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-03-27 23:56:05 +0000

    Added admin html page to show the messages and filter them by status

--- contact/models.py
+++ contact/models.py
@@ -1,18 +1,26 @@
 from django.db import models
 
+
 class Contact(models.Model):
-    STATUS_CHOICES = (
-        ('unread', 'Unread'),
-        ('read', 'Read'),
-        ('handled', 'Handled'),
-    )
+    UNREAD = 'unread'
+    READ = 'read'
+    HANDLED = 'handled'
+    STATUS_CHOICES = [
+        (UNREAD, 'Unread'),
+        (READ, 'Read'),
+        (HANDLED, 'Handled'),
+    ]
 
     subject = models.CharField(max_length=100)
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='unread')
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default=UNREAD,
+    )
 
     def __str__(self):
         return self.name
--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import contact, contact_success
+from .views import contact, contact_success, contact_admin
 
 urlpatterns = [
     path('contact/', contact, name='contact'),
     path('contact/success/', contact_success, name='contact_success'),
+    path('admin/contact/', contact_admin, name='contact_admin'),
 ]
+
