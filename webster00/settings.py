d871ff894323cb2d140421fd3f0c253de25ffc6a
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-03-27 17:53:26 +0000

    Only display add to wishlist if user is logged in

--- webster00/settings.py
+++ webster00/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
+#'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['webster00.herokuapp.com', 'localhost']
 
