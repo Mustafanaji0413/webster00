cd629ce42189fb27076b61947c3158a30e304174
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-03-27 23:30:11 +0000

    create a new contact view to get the contact messages from the database and display them in a html template

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.conf import settings
 from .forms import ContactForm
+from django.contrib.auth.decorators import login_required
+from .models import Contact
 
+@login_required
+def contact_admin(request):
+    if request.method == 'POST':
+        message_id = request.POST.get('message_id')
+        status = request.POST.get('status')
+        if message_id and status:
+            contact = Contact.objects.filter(id=message_id).first()
+            if contact:
+                contact.status = status
+                contact.save()
+    messages = Contact.objects.all().order_by('-created_at')
+    status_filter = request.GET.get('status_filter', 'unread')
+    if status_filter == 'read':
+        messages = messages.filter(status='read')
+    elif status_filter == 'handled':
+        messages = messages.filter(status='handled')
+    else:
+        messages = messages.filter(status='unread')
+    return render(request, 'contact_admin.html', {'messages': messages, 'status_filter': status_filter})
+    
 
 def contact(request):
     if request.method == 'POST':
@@ -13,10 +35,12 @@ def contact(request):
             subject = form.cleaned_data['subject']
             message = form.cleaned_data['message']
             send_mail(subject, message, email, [settings.DEFAULT_FROM_EMAIL])
+            Contact.objects.create(name=name, email=email, subject=subject, message=message)
             return render(request, 'success.html')
     else:
         form = ContactForm()
     return render(request, 'contact.html', {'form': form})
+
 
 
 def contact_success(request):
